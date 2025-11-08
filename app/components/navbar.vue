4caf8baa535a963e535d88ae522b71a2b3890355
Matheus Mortari <matheus.felipe.19rt@gmail.com>
2025-11-08 18:29:45 -0300

    chore(css): temporarily remove group/transition/scale styles

--- app/components/navbar.vue
+++ app/components/navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="bg-card fixed top-0 left-0 z-50 flex w-full items-center justify-between border-b px-4 py-2 md:px-8">
+  <nav class="bg-card fixed top-0 left-0 z-50 flex w-full items-center justify-between border-b px-4 py-2">
     <Logo />
 
     <button class="btn" aria-label="Toggle Theme" @click="toggleTheme">
