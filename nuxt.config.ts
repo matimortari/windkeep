a59db3d9df58224ff5127b3534aff6a0663de33d
Matheus Mortari <matheus.felipe.19rt@gmail.com>
2026-06-12 16:36:31 -0300

    chore(deps): upgrade npm packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -53,7 +53,11 @@ export default defineNuxtConfig({
       { name: "JetBrains Mono", provider: "google", weights: ["400"] },
     ],
   },
-  icon: { mode: "svg", clientBundle: { scan: true } },
+  icon: {
+    mode: "svg",
+    serverBundle: "remote",
+    clientBundle: { scan: true },
+  },
   ogImage: { enabled: false },
   robots: { disallow: ["/admin", "/onboarding"] },
   site: { url: process.env.NUXT_PUBLIC_BASE_URL, name: "WindKeep" },
